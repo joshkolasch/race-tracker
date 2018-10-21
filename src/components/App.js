53d2fe9274ecc61d21e74cea4bffe682fd2bbd9c
joshkolasch <joshua.kolasch@gmail.com>
2018-10-21 00:45:50 -0700

    Add fake api functions - lightly tested

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import '../styles/App.css';
 import { getEvent, getCheckpoint } from '../utils/api';
 import { getUTCTime, callConvertTimeToSplit, isValidSplit, formatSplit } from '../utils/helpers';
+import { callGetEvent, callCreateEvent, callGetCheckpoint, callAddSplit, callRestartEvent, callStartEvent, callUpdateSplit, callUpdateEvent, callEditEvent } from '../secrets/tests'
 
 class App extends Component {
   render() {
@@ -14,7 +15,7 @@ class App extends Component {
           App starts here!
         </div>
         <div>
-          {console.log('result ', formatSplit('9:5:1:45'))}
+          {console.log('call from app', callEditEvent())}
         </div>
       </div>
       
--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -2,50 +2,40 @@
 //state should only need to contain 'event' and 'checkpoint'
 //the app only needs to be made aware of the current checkpoint
 //if someone wants to 
-let event = {
-  id: 2305,
+let testEvent = {
+  eventID: 2305,
   name: 'Sunny Hills Relay',
   numRunners: 25,
   numCheckpoints: 10,
-  startTime: '10:00:000',
-  checkpoints: [
-    {
-      id: 1,
-      runners: {
-        1: {
-          split: '10:20',
-          timestamp: '10:10:100'
-        },
-        23: {
-          split: '5:15',
-          timestamp: '10:05:200'
-        }
-      }
-    },
-    {
-      id: 2
-    },
-    {
-      id: 3
-    },
-  ]
+  startTime: '10:00:000'
 }
 
-let checkpoint = {
-  id: 1,
+/* */
+let testCheckpoint = {
+  checkpointID: 1,
   runners: {
     1: {
+      runnerID: 1234,
+      runnerNumber: 1,
       split: '10:20',
-      timestamp: '10:10:100'
+      timestamp: 1540084649946,
+      updateMethod: 'auto'
     },
     23: {
+      runnerID: 1235,
+      runerNumber: 23,
       split: '5:15',
-      timestamp: '10:05:200'
+      timestamp: 1540084688057,
+      updateMethod: 'manual'
     }
   }
 }
 
+let runID = 300;
+
 //store should look like the following
+//updateMethod refers to how the user added the split (this is important for a future feature)
+//if the user adjusts the event.startTime, the splits with 'auto' will all be updated
 /*
   store = {
     event: {stuff},
@@ -55,27 +45,184 @@ let checkpoint = {
     checkpoint: 3,
     runners: {
       1: {
+        runnerID: 1234,
+        runnerNumber: 1,
         split: '10:20',
-        timestamp: '10:10:100'
+        timestamp: '1540084649946',
+        updateMethod: 'auto'
       },
       23: {
+        runnerID: 1235, 
+        runnerNumber: 23,
         split: '5:15',
-        timestamp: '10:05:200'
+        timestamp: '1540084688057',
+        updateMethod: 'manual'
       }
     }
   }
-
 */
 
-//DONE: test these two functions to make sure they return the correct piece of state
-export function _getEvent () {
+
+//test: DONE
+export function _createEvent(event) {
   return new Promise((res, rej) => {
-    setTimeout(() => res({...event}), 1000)
+    const formattedEvent = formatEvent(event);
+    console.log('testEvent before in db', testEvent)
+    setTimeout(() => {
+      testEvent = {
+        ...formattedEvent,
+      }
+      console.log('testEvent in db', testEvent)
+      res(formattedEvent);
+    }, 1000)
   })
 }
 
+//test: DONE
+export function _getEvent () {
+  return new Promise((res, rej) => {
+    setTimeout(() => res({...testEvent}), 1000)
+  })
+}
+
+//test: DONE
+export function _startEvent(eventID, startTime) {
+  console.log('event before startEvent', testEvent);
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      testEvent = {
+        ...testEvent,
+        startTime: startTime
+      }
+      console.log('event after startEvent', testEvent);
+      res();
+    }, 1000)
+  })
+}
+
+//test: DONE
+export function _restartEvent(eventID) {
+  console.log('event before restartEvent', testEvent);
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      testEvent = {
+        ...testEvent,
+        startTime: ''
+      }
+      console.log('event after restartEvent', testEvent);
+    }, 1000)
+  })
+}
+
+//test: DONE
+export function _editEvent(event) {
+  return new Promise((res, rej) => {
+    console.log('before edit', testEvent);
+    setTimeout(() => {
+      testEvent = {
+        ...event
+      }
+      console.log('after edit', testEvent);
+      res(event);
+    }, 1000)
+  })
+}
+
+//test: DONE
 export function _getCheckpoint (eventID, checkpointID) {
   return new Promise((res, rej) => {
-    setTimeout(() => res({...checkpoint}), 1000)
+    setTimeout(() => res({...testCheckpoint}), 1000)
   })
+}
+
+
+//test: DONE
+export function _addSplit(eventID, checkpointID, runner) {
+  return new Promise((res, rej) => {
+    const formattedRunner = formatRunner(runner);
+    setTimeout(() => {
+      testCheckpoint = {
+        ...testCheckpoint,
+        runners: {
+          ...testCheckpoint.runners,
+          [formattedRunner.runnerNumber]: formattedRunner
+        }
+        
+      }
+      res(formattedRunner);
+    }, 1000)
+  })
+}
+
+//test: DONE
+export function _updateSplit(eventID, checkpointID, runner) {
+  return new Promise((res, rej) => {
+    console.log('before update split', testCheckpoint)
+    setTimeout(() => {
+      testCheckpoint = {
+        ...testCheckpoint,
+        runners: {
+          ...testCheckpoint.runners,
+          [runner.runnerNumber]: {
+            ...runner
+          }
+        }
+      }
+      console.log('after update split', testCheckpoint);
+      res();
+    }, 1000)
+  })
+}
+
+
+//test: DONE
+function formatRunner(runner) {
+  const { runnerNumber, split, updateMethod } = runner;
+  const runnerID = generateRunnerID();
+  const timestamp = getUTCTime();
+
+  let newRunner = {
+    runnerID,
+    runnerNumber,
+    split,
+    timestamp,
+    updateMethod
+  };
+
+  return newRunner;
+}
+
+
+//test: DONE
+//used for creating an event
+function formatEvent(event) {
+  const { name, numRunners, numCheckpoints, startTime } = event;
+  //TODO:
+  // do some sort of variable validation here
+
+  let newEvent = {
+    eventID: generateUID(),
+    name,
+    numRunners,
+    numCheckpoints,
+    startTime,
+  }
+
+  return newEvent;
+}
+
+//TODO: test -
+function generateUID () {
+  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+}
+
+//test: DONE
+export function getUTCTime() {
+  return new Date().getTime();
+}
+
+
+function generateRunnerID() {
+  runID += 1;
+  return runID;
 }
